70a4c1dd19e1a7f995934e3f99bc5ca894e670e0
kristojorg <kristo.jorgenson@gmail.com>
2018-02-16 09:07:08 -0500

    added flow

--- front/src/App.js
+++ front/src/App.js
@@ -3,11 +3,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from './AppBar';
 
-class App extends Component<null, null> {
+type P = {};
+class App extends Component<P> {
   render() {
     return (
       <div className="App">
+        <AppBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
