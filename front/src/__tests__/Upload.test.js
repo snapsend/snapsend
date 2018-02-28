54ce2d37ccc9d71c0f039d36575d42d8d0382b78
Shreya Jain <shreya94jain@gmail.com>
2018-02-28 13:18:22 -0500

    Test help

--- front/src/__tests__/Upload.test.js
+++ front/src/__tests__/Upload.test.js
@@ -1,5 +1,9 @@
 it('When image upload succeeds, it returns without error', () => {
   // something
+	mock(uploadImage, filestack.upload, ()=> return {status: "OK"});
+
+	const result = uploadImage('image');
+	expect(result).toBe();
 });
 
 it('When image upload fails, it returns the error code', () => {
@@ -13,3 +17,21 @@ it('When input is not a file, it returns ___ error', () => {
 it('When image is wrong format, it returns ___ error', () => {
   // something
 });
+
+function uploadImage(src){
+	uploadingImage = true;
+	const res = filestack.upload(src);
+	
+	if (error) {
+		return "There was an error";
+	}
+	else if (res === 'undefined'){
+		return "Upload failed";
+	}
+	else if (status === "OK"){
+		return "Upload succeeded"
+	}
+	return "Upload failed";
+}
+
+
