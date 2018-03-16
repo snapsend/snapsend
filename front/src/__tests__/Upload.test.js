29fcb714edb3cc1fdcd82d449ccd281c3ddf004d
kristojorg <kristo.jorgenson@gmail.com>
2018-03-16 13:00:57 -0400

    added failing tests to define uploadImage API

--- front/src/__tests__/Upload.test.js
+++ front/src/__tests__/Upload.test.js
@@ -9,42 +9,43 @@ import {
 const TEST_IMAGE = './data/test.jpg';
 
 describe('upload to filestack', () => {
+  test('When input is not an array of strings, it returns proper error', () => {
+    expect.assertions(1);
+    return expect(uploadImage(TEST_IMAGE)).rejects.toMatchSnapshot();
+  });
+
   test('if upload succeeds, it should return the image payload', () => {
     return uploadImage(TEST_IMAGE).then(res => {
       return expect(res).toEqual(successResult);
     });
   });
 
-  test('When image upload fails, it returns the standard error', () => {
-    // expect.assertions(1);
+  test('When upload fails, it returns the right error', () => {
+    expect.assertions(1);
     upload.mockImplementationOnce(rejectWithError);
 
     return expect(uploadImage(TEST_IMAGE)).rejects.toMatchSnapshot();
   });
 
-  test('When input is not a file, it returns ___ error', () => {
-    // something
+  test('When input is not a file, it returns the right error', () => {
+    expect.assertions(1);
+    const notAFile = '../asdfj';
+    return expect(uploadImage(notAFile)).rejects.toMatchSnapshot();
   });
 
-  test('When image is wrong format, it returns ___ error', () => {
-    // something
+  test('When image is wrong format, it returns the right error', () => {
+    expect.assertions(1);
+    const fileInWrongFormat = './data/notAnImage.rtf';
+    return expect(uploadImage(fileInWrongFormat)).rejects.toMatchSnapshot();
+  });
+  test('When input is multiple files, it returns an array of promises', () => {
+    expect.assertions(3);
+    const arrayOfImages = [
+      TEST_IMAGE,
+      './data/test1.jpeg',
+      './data/test2.jpeg',
+    ];
+    const result = uploadImage(arrayOfImages);
+    return result.forEach(res => expect(res).resolves.toEqual(successResult));
   });
 });
-
-// function uploadImage(src){
-// 	uploadingImage = true;
-// 	const res = filestack.upload(src);
-
-// 	if (error) {
-// 		return "There was an error";
-// 	}
-// 	else if (res === 'undefined'){
-// 		return "Upload failed";
-// 	}
-// 	else if (status === "OK"){
-// 		return "Upload succeeded"
-// 	}
-// 	return "Upload failed";
-// }
-
-test('placeholder test', () => {});
