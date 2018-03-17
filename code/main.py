3c83f77015b36ee7fad626183a8e2ccf8d4cf9d8
maitrishroff1995 <maitrishroff1995@gmail.com>
2018-03-17 14:53:11 -0400

    changed connction to DB function

--- code/main.py
+++ code/main.py
@@ -34,23 +34,25 @@ CLOUDSQL_PASSWORD = 'snapsend'
 def connect_to_cloudsql():
     # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
     # will be set to 'Google App Engine/version'.
-    # if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-    #     # Connect using the unix socket located at
-    #     # /cloudsql/cloudsql-connection-name.
-    #     cloudsql_unix_socket = os.path.join(
-    #         '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        print("inside if")
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
 
-    #     db = MySQLdb.connect(
-    #         unix_socket=cloudsql_unix_socket,
-    #         user=CLOUDSQL_USER,
-    #         passwd=CLOUDSQL_PASSWORD)
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
 
+    else:
+        print("inside else")
+        db = MySQLdb.connect(
+            host='35.231.24.52', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
     # else:
-    #     db = MySQLdb.connect(
-    #         host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
-    # else:
-    db = MySQLdb.connect(
-        host='35.231.24.52', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+    # db = MySQLdb.connect(
+    #     host='35.231.24.52', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
     return db
 
 
