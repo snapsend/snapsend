67204ed9a308226adaf727ebffdbe36f5232a97b
shreyajn <shreya94jain@gmail.com>
2018-03-18 12:37:38 -0400

    image ID auto generated and GET input changed

--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
-from google.appengine.ext import vendor
-vendor.add('lib')
+#from google.appengine.ext import vendor
+#vendor.add('lib')
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 
@@ -20,13 +20,13 @@ import MySQLdb
 #     verify=False)
 # commiting get
 # These environment variables are configured in app.yaml.
-CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
-CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
-CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+#CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+#CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+#CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
 
-# CLOUDSQL_CONNECTION_NAME = 'flask-snapsend:us-east1:snapsend-mysql'
-# CLOUDSQL_USER = 'root'
-# CLOUDSQL_PASSWORD = 'snapsend'
+CLOUDSQL_CONNECTION_NAME = 'flask-snapsend:us-east1:snapsend-mysql'
+CLOUDSQL_USER = 'root'
+CLOUDSQL_PASSWORD = 'snapsend'
 
 
 def connect_to_cloudsql():
@@ -38,9 +38,9 @@ def connect_to_cloudsql():
     cloudsql_unix_socket = os.path.join('/cloudsql', CLOUDSQL_CONNECTION_NAME)
 
     db = MySQLdb.connect(
-        unix_socket=cloudsql_unix_socket,
-        user=CLOUDSQL_USER,
-        passwd=CLOUDSQL_PASSWORD)
+      unix_socket=cloudsql_unix_socket,
+      user=CLOUDSQL_USER,
+      passwd=CLOUDSQL_PASSWORD)
 
   else:
     db = MySQLdb.connect(
@@ -73,118 +73,125 @@ def showDatabases():
   return response
 
 
-@app.route('/envelope', methods=['POST', 'GET'])
-def envelope():
-    if request.method == 'POST':
-        loaded_r = request.get_json()
-        r = json.dumps(loaded_r)
-        loaded_r = json.loads(r)
-        env_name = loaded_r['envelopeName']
-        rec_name = loaded_r['recipientName']
-        sender_name = loaded_r['senderName']
-        all_images = loaded_r['images']
+@app.route('/envelope', methods=['POST'])
+def postenvelope():
+  if request.method =="POST":
+    loaded_r = request.get_json()
+    print loaded_r
+    #loaded_r = {"envelopeName" : "MyEllmnvelope","recipientName": "Johlmlmn","senderName": "Mary",
+     #             "images": [{"url": "blah1", "filename": "pic1.jpg"},{"url": "blah2","filename": "pic2.jpg"}]}
+    r = json.dumps(loaded_r)
+    loaded_r = json.loads(r)
+    env_name = loaded_r['envelopeName']
+    rec_name = loaded_r['recipientName']
+    sender_name = loaded_r['senderName']
+    all_images = loaded_r['images']
+    print env_name,rec_name,sender_name,all_images
+    db = connect_to_cloudsql()
 
-        db = connect_to_cloudsql()
+    cursor = db.cursor()
+    # sql_user_query = 'INSERT INTO snapsend.User (email, password, uname) values ("'+ email +'", "' + pwd + '", "' + uname + '");'
+    sql_env_query = 'INSERT INTO snapsend.Envelope (ename) values ("' + env_name + '");'
+    cursor.execute(sql_env_query)
+    db.commit()
+    cursor.close()
 
-        cursor = db.cursor()
-        # sql_user_query = 'INSERT INTO snapsend.User (email, password, uname) values ("'+ email +'", "' + pwd + '", "' + uname + '");'
-        sql_env_query = 'INSERT INTO snapsend.Envelope (ename) values ("' + env_name + '");'
-        cursor.execute(sql_env_query)
+    cursor = db.cursor()
+    sql_get_env_id = 'SELECT max(envelopeID) from snapsend.Envelope;'
+    cursor.execute(sql_get_env_id)
+
+    j = ""
+    for r in cursor.fetchall():
+      j += str(r[0])
+    j = int(j)
+    cursor.close()
+
+    try:
+      cursor = db.cursor()
+      for i in range(len(all_images)):
+        curr_dict = all_images[i]
+        #a = curr_dict['imageId']
+        b = curr_dict['url']
+        c = curr_dict['filename']
+        sql_image_query = 'INSERT INTO snapsend.Image (inenvID, imagelink, filename) values (' + str(
+            j)  + ', "' + b + '", "' + c + '");'
+        cursor.execute(sql_image_query)
         db.commit()
-        cursor.close()
 
-        cursor = db.cursor()
-        sql_get_env_id = 'SELECT max(envelopeID) from snapsend.Envelope;'
-        cursor.execute(sql_get_env_id)
+      print("success")
+    except Exception as e:
+      print("error")
 
-        j = ""
-        for r in cursor.fetchall():
-          j += str(r[0])
-        j = int(j)
-        cursor.close()
+    cursor.close()
 
-        try:
-          cursor = db.cursor()
-          for i in range(len(all_images)):
-            curr_dict = all_images[i]
-            a = curr_dict['imageId']
-            b = curr_dict['url']
-            c = curr_dict['filename']
-            sql_image_query = 'INSERT INTO snapsend.Image (inenvID, imageID, imagelink, filename) values (' + str(
-                j) + ', ' + str(a) + ', "' + b + '", "' + c + '");'
-            cursor.execute(sql_image_query)
-            db.commit()
-
-          print("success")
-        except Exception as e:
-          print("error")
-
-        cursor.close()
-
-        loaded_r['envelopeID'] = j
-        response = make_response(json.dumps(loaded_r))
-        response.headers['Content-Type'] = 'text/json'
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        return response
-    
-    elif request.method == 'GET':
-        
-        url = request.url
-        url = url.split('?')
-        k = url[1].split('=')
-        loaded_r = {"envelopeId":k[1] }
-        r = json.dumps(loaded_r)
-        loaded_r = json.loads(r)
-        #print loaded_r
-        env_id = loaded_r['envelopeId']
-
-        db = connect_to_cloudsql()
-
-        cursor = db.cursor()
-
-        sql_get_env = 'SELECT ename, sender, recipient, createddate FROM snapsend.Envelope WHERE envelopeID = ' + str(env_id) + ';'
-
-        #sql_count_images = 'SELECT COUNT (imageID) FROM snapsend.Image WHERE inenvID = ' + str(env_id) + ';'
-
-        sql_get_images = 'SELECT imageID, imagelink, filename FROM snapsend.Image WHERE inenvID = ' + str(env_id) + ';'
-        payload = ""
-        env_out = {}
-        try:
-            cursor.execute(sql_get_env)
-            result = cursor.fetchall()
-         
-            env_out = {"envelopeId": env_id , "envelopeName" :  result[0][0] , "recipientName": result[0][2] , "senderName":  result[0][1], "created date":  result[0][3] }
-
-            #user_id = result[0]
-                
-            #img_ct = cursor.execute(sql_count_images)
-
-            cursor.execute(sql_get_images)
-            imgres = cursor.fetchall()
-            #print imgres
-            #payload = "" 
-            img_arr = []
-            img_out = {}
-
-            for imgs in imgres:
-                img_out = {"imageId": imgs[0], "url": imgs[1], "filename": imgs[2]}
-                img_arr.append(img_out)
-                img_out = {}
-            #print img_arr
-            payload = env_out
-            payload["images"]=img_arr
-            #print payload
-            return jsonify(payload)
+    loaded_r['envelopeID'] = j
+    response = make_response(json.dumps(loaded_r))
+    response.headers['Content-Type'] = 'text/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
         
-            print("success")    
-        except Exception as e:
-            print("error")
+        
 
-        response = make_response(payload)
-        response.headers['Content-Type'] = 'text/json'
-        return response
+@app.route('/envelope/<int:env_id>', methods=['GET'])
+def getenvelope(env_id):
+  print env_id
+  #url = request.url
+  #url = url.split('?')
+  #k = url[1].split('=')
+  loaded_r = {"envelopeId":env_id }
+  r = json.dumps(loaded_r)
+  loaded_r = json.loads(r)
+  #print loaded_r
+  env_id = loaded_r['envelopeId']
 
+  db = connect_to_cloudsql()
+
+  cursor = db.cursor()
+
+  sql_get_env = 'SELECT ename, sender, recipient, createddate FROM snapsend.Envelope WHERE envelopeID = ' + str(env_id) + ';'
+
+  #sql_count_images = 'SELECT COUNT (imageID) FROM snapsend.Image WHERE inenvID = ' + str(env_id) + ';'
+
+  sql_get_images = 'SELECT imageID, imagelink, filename FROM snapsend.Image WHERE inenvID = ' + str(env_id) + ';'
+  payload = ""
+  env_out = {}
+  try:
+      cursor.execute(sql_get_env)
+      result = cursor.fetchall()
+   
+      env_out = {"envelopeId": env_id , "envelopeName" :  result[0][0] , "recipientName": result[0][2] , "senderName":  result[0][1], "created date":  result[0][3] }
+
+      #user_id = result[0]
+          
+      #img_ct = cursor.execute(sql_count_images)
+
+      cursor.execute(sql_get_images)
+      imgres = cursor.fetchall()
+      #print imgres
+      #payload = "" 
+      img_arr = []
+      img_out = {}
+
+      for imgs in imgres:
+          img_out = {"imageId": imgs[0], "url": imgs[1], "filename": imgs[2]}
+          img_arr.append(img_out)
+          img_out = {}
+      #print img_arr
+      payload = env_out
+      payload["images"]=img_arr
+      #print payload
+      return jsonify(payload)
+
+  
+      print("success")    
+  except Exception as e:
+      print("error")
+
+  response = make_response(payload)
+  response.headers['Content-Type'] = 'text/json'
+  
+  return response
 
 # @app.route('/getEnvelope')
 # def getEnvelope():
