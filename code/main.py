3581d6c0ca576b5a2d179123b1f6dcedb6922cad
kristojorg <kristo.jorgenson@gmail.com>
2018-03-17 21:49:07 -0400

    added flask-cors

--- code/main.py
+++ code/main.py
@@ -1,15 +1,15 @@
 from google.appengine.ext import vendor
 vendor.add('lib')
 from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 import os
 import json
 import sys
 import os
 import MySQLdb
-
-
 
 # dynamodb = boto3.resource(
 #     'dynamodb',
@@ -23,120 +23,117 @@ import MySQLdb
 CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
 CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
 CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
 # CLOUDSQL_CONNECTION_NAME = 'flask-snapsend:us-east1:snapsend-mysql'
 # CLOUDSQL_USER = 'root'
 # CLOUDSQL_PASSWORD = 'snapsend'
 
 
 def connect_to_cloudsql():
-    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
-    # will be set to 'Google App Engine/version'.
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-        # Connect using the unix socket located at
-        # /cloudsql/cloudsql-connection-name.
-        cloudsql_unix_socket = os.path.join(
-            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+  # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+  # will be set to 'Google App Engine/version'.
+  if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+    # Connect using the unix socket located at
+    # /cloudsql/cloudsql-connection-name.
+    cloudsql_unix_socket = os.path.join('/cloudsql', CLOUDSQL_CONNECTION_NAME)
 
-        db = MySQLdb.connect(
-            unix_socket=cloudsql_unix_socket,
-            user=CLOUDSQL_USER,
-            passwd=CLOUDSQL_PASSWORD)
+    db = MySQLdb.connect(
+        unix_socket=cloudsql_unix_socket,
+        user=CLOUDSQL_USER,
+        passwd=CLOUDSQL_PASSWORD)
 
-    else:
-        db = MySQLdb.connect(
-            host='35.231.24.52', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
-    # else:
-    # db = MySQLdb.connect(
-    #     host='35.231.24.52', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
-    return db
+  else:
+    db = MySQLdb.connect(
+        host='35.231.24.52', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+  # else:
+  # db = MySQLdb.connect(
+  #     host='35.231.24.52', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+  return db
 
 
 @app.route('/')
 def index():
-    return "Hello, World (lets see how long a change takes III)!"
+  return "Hello, World (lets see how long a change takes III)!"
+
 
 @app.route('/databases')
 def showDatabases():
-    """Simple request handler that shows all of the MySQL SCHEMAS/DATABASES."""
-    db = connect_to_cloudsql()
+  """Simple request handler that shows all of the MySQL SCHEMAS/DATABASES."""
+  db = connect_to_cloudsql()
 
-    cursor = db.cursor()
-    cursor.execute('SHOW SCHEMAS')
-    res = ""
-    for r in cursor.fetchall():
-        res+= ('{}\n'.format(r[0]))
+  cursor = db.cursor()
+  cursor.execute('SHOW SCHEMAS')
+  res = ""
+  for r in cursor.fetchall():
+    res += ('{}\n'.format(r[0]))
 
-    response = make_response(res)
-    response.headers['Content-Type'] = 'text/json'
+  response = make_response(res)
+  response.headers['Content-Type'] = 'text/json'
 
-    return response
-
+  return response
 
 
 @app.route('/envelope', methods=['POST', 'GET'])
-def envelope(): 
-    if request.method == 'POST':
-        loaded_r = request.get_json()
-        r = json.dumps(loaded_r)
-        loaded_r = json.loads(r)
-        env_name = loaded_r['envelopeName']
-        rec_name = loaded_r['recipientName']
-        sender_name = loaded_r['senderName']
-        all_images = loaded_r['images']
+def envelope():
+  if request.method == 'POST':
+    loaded_r = request.get_json()
+    r = json.dumps(loaded_r)
+    loaded_r = json.loads(r)
+    env_name = loaded_r['envelopeName']
+    rec_name = loaded_r['recipientName']
+    sender_name = loaded_r['senderName']
+    all_images = loaded_r['images']
 
-        db = connect_to_cloudsql()
+    db = connect_to_cloudsql()
 
-        cursor = db.cursor()
-        # sql_user_query = 'INSERT INTO snapsend.User (email, password, uname) values ("'+ email +'", "' + pwd + '", "' + uname + '");'
-        sql_env_query = 'INSERT INTO snapsend.Envelope (ename) values ("' + env_name + '");'
-        cursor.execute(sql_env_query)
+    cursor = db.cursor()
+    # sql_user_query = 'INSERT INTO snapsend.User (email, password, uname) values ("'+ email +'", "' + pwd + '", "' + uname + '");'
+    sql_env_query = 'INSERT INTO snapsend.Envelope (ename) values ("' + env_name + '");'
+    cursor.execute(sql_env_query)
+    db.commit()
+    cursor.close()
+
+    cursor = db.cursor()
+    sql_get_env_id = 'SELECT max(envelopeID) from snapsend.Envelope;'
+    cursor.execute(sql_get_env_id)
+
+    j = ""
+    for r in cursor.fetchall():
+      j += str(r[0])
+    j = int(j)
+    cursor.close()
+
+    try:
+      cursor = db.cursor()
+      for i in range(len(all_images)):
+        curr_dict = all_images[i]
+        a = curr_dict['imageId']
+        b = curr_dict['url']
+        c = curr_dict['filename']
+        sql_image_query = 'INSERT INTO snapsend.Image (inenvID, imageID, imagelink, filename) values (' + str(
+            j) + ', ' + str(a) + ', "' + b + '", "' + c + '");'
+        cursor.execute(sql_image_query)
         db.commit()
-        cursor.close()
 
-        cursor = db.cursor()
-        sql_get_env_id = 'SELECT max(envelopeID) from snapsend.Envelope;'
-        cursor.execute(sql_get_env_id)
+      print("success")
+    except Exception as e:
+      print("error")
 
-        
-        j = ""
-        for r in cursor.fetchall():
-            j += str(r[0])
-        j = int(j)
-        cursor.close()
+    cursor.close()
 
-        try:
-            cursor = db.cursor()
-            for i in range(len(all_images)):
-                curr_dict = all_images[i]
-                a = curr_dict['imageId']
-                b = curr_dict['url']
-                c = curr_dict['filename']
-                sql_image_query = 'INSERT INTO snapsend.Image (inenvID, imageID, imagelink, filename) values (' + str(j) +', '+ str(a) +', "' + b + '", "' + c + '");'
-                cursor.execute(sql_image_query)
-                db.commit()
-            
-            print("success")    
-        except Exception as e:
-            print("error")
-        
-        cursor.close()
+    loaded_r['envelopeID'] = j
+    response = make_response(json.dumps(loaded_r))
+    response.headers['Content-Type'] = 'text/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
-        loaded_r['envelopeID'] = j
-        response = make_response(json.dumps(loaded_r))
-        response.headers['Content-Type'] = 'text/json'
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        return response
-
-    elif(request.method == 'GET'):
-        return "hi"
-
-
-
+  elif (request.method == 'GET'):
+    return "hi"
 
 
 # @app.route('/getEnvelope')
 # def getEnvelope():
-    
+
 # j_in = {
 #     "envelopeId": 1501
 #     }
@@ -162,12 +159,12 @@ def envelope():
 #        # env_out = {"envelopeId": env_id, "envelopeName" : result[1], "recipientName": result[3], "senderName": result[2], "created date": result[4]}
 
 #        # user_id = result[0]
-        
+
 #        #  img_ct = cursor.execute(sql_count_images)
-    
+
 #        #  cursor.execute(sql_get_images)
 #        #  imgres = cursor.fetchall()
-    
+
 #        # img_arr = []
 #        # img_out = {}
 
@@ -180,19 +177,15 @@ def envelope():
 
 #        # return jsonify(payload)
 
-        
-#         print("success")    
+#         print("success")
 #     except Exception as e:
 #         print("error")
-    
 
 #     response = make_response(payload)
 
 #     response.headers['Content-Type'] = 'text/json'
 
-#     return response  
-
-
+#     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
