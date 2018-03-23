0f0bf81dfd3a224ffdd15850e088f9745ae9f338
shreyajn <shreya94jain@gmail.com>
2018-03-23 13:50:26 -0400

    changing config

--- compile.sh
+++ compile.sh
@@ -2,27 +2,33 @@
 
 if [ "${CIRCLE_BRANCH}" == "dev" ]
 then
+  gcloud config set project $GCLOUD_PROJECT_DEV
   # Retrieve our secrets from the CircleCI environment
   echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
   mkdir -p lib
+  pip install --upgrade pip
   pip install -r server/requirements.txt -t lib
   gcloud --quiet components update app
   gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
-  gcloud config set project $GCLOUD_PROJECT_DEV
+  
 
 elif [ "${CIRCLE_BRANCH}" == "master" ]
 then
+  gcloud config set project $GCLOUD_PROJECT
   echo $CLIENT_SECRET1 | base64 --decode > ${HOME}/client-secret.json
   mkdir -p lib
+  pip install --upgrade pip
   pip install -r server/requirements.txt -t lib
   gcloud --quiet components update app
   gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
-  gcloud config set project $GCLOUD_PROJECT
+  
 else
+  gcloud config set project $GCLOUD_PROJECT_DEV
   echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
   mkdir -p lib
+  pip install --upgrade pip
   pip install -r server/requirements.txt -t lib
   gcloud --quiet components update app
   gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
-  gcloud config set project $GCLOUD_PROJECT_DEV
+  
 fi
