ff9f5a4db2e7fa7fece37b23168b6f9943e77b3c
shreyajn <shreya94jain@gmail.com>
2018-03-23 13:44:19 -0400

    changing configs

--- compile.sh
+++ compile.sh
@@ -2,28 +2,27 @@
 
 if [ "${CIRCLE_BRANCH}" == "dev" ]
 then
-  curl -o $HOME/google_appengine_1.9.30.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.30.zip
-  unzip -q -d $HOME $HOME/google_appengine_1.9.30.zip
+  # Retrieve our secrets from the CircleCI environment
   echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
   mkdir -p lib
   pip install -r server/requirements.txt -t lib
   gcloud --quiet components update app
   gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
-  gcloud config set project $GCLOUD_PROJECT_ID
-  cd front && npm install
+  gcloud config set project $GCLOUD_PROJECT_DEV
+
 elif [ "${CIRCLE_BRANCH}" == "master" ]
 then
-  export NODE_ENV=production
-  export MONGODB_URI=${PRODUCTION_DB}
-  npm run build
+  echo $CLIENT_SECRET1 | base64 --decode > ${HOME}/client-secret.json
+  mkdir -p lib
+  pip install -r server/requirements.txt -t lib
+  gcloud --quiet components update app
+  gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
+  gcloud config set project $GCLOUD_PROJECT
 else
-  curl -o $HOME/google_appengine_1.9.30.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.30.zip
-  unzip -q -d $HOME $HOME/google_appengine_1.9.30.zip
   echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
   mkdir -p lib
   pip install -r server/requirements.txt -t lib
   gcloud --quiet components update app
   gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
-  gcloud config set project $GCLOUD_PROJECT_ID
-  cd front && npm install
+  gcloud config set project $GCLOUD_PROJECT_DEV
 fi
