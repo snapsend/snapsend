52abffcc327550a0e137b90b1fc86cbf8f827f6c
maitrishroff1995 <maitrishroff1995@gmail.com>
2018-03-30 23:30:37 -0400

    added vendor back

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-#from google.appengine.ext import vendor
-#vendor.add('lib')
+from google.appengine.ext import vendor
+vendor.add('lib')
 from flask import Flask, request, jsonify, make_response
+
 #needed for front and backend to work together
 from flask_cors import CORS
 import flask
