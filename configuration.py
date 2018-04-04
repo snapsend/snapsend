2f69d0e108381bd6e6249e4979db7e2abb740bd0
kristojorg <kristo.jorgenson@gmail.com>
2018-04-04 12:23:14 -0400

    mysql

--- configuration.py
+++ configuration.py
@@ -3,23 +3,27 @@
 # Source: https://github.com/realpython/discover-flask/blob/master/config.py
 from flask_sqlalchemy import SQLAlchemy
 
+
 class BaseConfig(object):
-	DEBUG = True
-	TESTING = False
-	SECRET_KEY = 'snapsend'
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:snapsend_rocks@35.231.24.52/snapsend'
-	# SQLALCHEMY_DATABASE_URI = 'mysql://root:snapsend_rocks@35.231.24.52/snapsend'
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SQLALCHEMY_ECHO = True
+  DEBUG = True
+  TESTING = False
+  SECRET_KEY = 'snapsend'
+  SQLALCHEMY_DATABASE_URI = 'mysql://root:snapsend_rocks@35.231.24.52/snapsend'
+  # SQLALCHEMY_DATABASE_URI = 'mysql://root:snapsend_rocks@35.231.24.52/snapsend'
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SQLALCHEMY_ECHO = True
+
 
 class TestConfig(BaseConfig):
-	DEBUG = True
-	TESTING = True
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-	SQLALCHEMY_ECHO = False
+  DEBUG = True
+  TESTING = True
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+  SQLALCHEMY_ECHO = False
+
 
 class DevelopmentalConfig(BaseConfig):
-	DEBUG = True
+  DEBUG = True
+
 
 class ProductionConfig(BaseConfig):
-	DEBUG = False
+  DEBUG = False
